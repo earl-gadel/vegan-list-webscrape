5fd17a940c3a672bb818b4e4d6cb4431762ac7f0
Earl <earljg@gmail.com>
2018-08-05 14:30:08 -0500

    created list of non-vegan foods from webscrape

---
+++ scrape-vegan-list.py
@@ -0,0 +1,16 @@
+from lxml import html
+from lxml import etree
+import requests
+
+page = requests.get('https://www.peta.org/living/food/animal-ingredients-list/')
+tree = html.fromstring(page.content)
+
+non_vegan_list = tree.xpath('//b/text()')
+non_vegan_list = [s.replace('.', '') for s in non_vegan_list]
+
+list_file = open('the_list.txt', 'w')
+for item in non_vegan_list:
+    list_file.write('%s\n' % item)
+
+print (type(non_vegan_list))
+print ('List:', non_vegan_list)
